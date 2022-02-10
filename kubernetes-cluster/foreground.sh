4f9a0fcc903f584a76ea9e96dd944391992dc290
txabarri <95035220+txabarri@users.noreply.github.com>
2022-02-10 16:29:13 +0100

    cambios script

---
+++ kubernetes-cluster/foreground.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+kubeadm init --kubernetes-version $(kubeadm version -o short)
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
